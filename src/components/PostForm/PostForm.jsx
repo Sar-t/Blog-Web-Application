4c7c16cf22314df500d80890316631eaf4d4b28f
Sarthak Tomar <sarthaktomar345@gmail.com>
2025-07-18 21:40:10 +0530

    Update environment variables and disable input in PostForm component

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -104,6 +104,7 @@ export default function PostForm({ post }) {
               shouldValidate: true,
             })
           }
+          disabled
         />
         <RTE
           label="Content"
